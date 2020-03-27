7a4b2973eb9d5d65a61ce604a8588f82e3411951
Harsha vardhan Chokkakula <harsha.chokkakula@wipro.com>
2020-03-27 13:01:23 +0530

    ...

--- onlinebookstore/onlinebookstore/src/main/resources/data.sql
+++ onlinebookstore/onlinebookstore/src/main/resources/data.sql
@@ -1,3 +1,274 @@
-insert into category values(1,'text books');
-insert into tbl_book values(1,true,'2020-03-18','Book to learn c lang','assets\images\books\text-100.jpg','c language','txt-100',600.00,100,NULL,1);
-insert into tbl_book values(2,true,'2020-03-18','Book to learn c+ lang','assets\images\books\text-100.jpg','c+ language','txt-101',500.00,50,NULL,1);
+INSERT INTO category(category_name) VALUES ('Web Development');
+INSERT INTO category(category_name) VALUES ('Programming');
+INSERT INTO category(category_name) VALUES ('Databases');
+INSERT INTO category(category_name) VALUES ('Administration');
+insert into tbl_book values
+(
+	1,
+	true,
+	'2020-03-18',    
+	'Business users familiar with Base SAS programming can now learn Python by example. You will learn via examples that map SAS programming constructs and coding patterns into their Python equivalents.',
+	'assets/images/webdevelopment/webdevelopment-100.jpg',
+	'Python for SAS Users',
+	'webdevelopment-100',
+	600.00,
+	100,
+	NULL,
+	1);
+insert into tbl_book values(
+	2,
+	true,
+	'2020-03-18',
+	'Learn how to make better decisions and write cleaner Ruby code. This book shows you how to avoid messy code that is hard to test and which cripples productivity.',
+	'assets/images/webdevelopment/webdevelopment-102.jpg',
+	'Clean Ruby', 
+	'webdevelopment-102',
+	800.00,
+	50,
+	NULL,
+	1);
+insert into tbl_book VALUES 
+(
+	3,
+	true,
+	'2020-03-18',
+    'Professional developers know the many benefits of writing application code that’s clean, well-organized, and easy to maintain.',
+    'assets/images/webdevelopment/webdevelopment-103.jpg',
+    'Practices of the Python Pro', 
+    'webdevelopment-103', 
+    900.00,
+    100,
+    NULL,
+    1);
+insert into tbl_book VALUES 
+(
+	4,
+	true,
+	'2020-03-18',
+	'In 2017, consumers downloaded 178 billion apps, and analysts predict growth to 258 billion by 2022. Mobile customers are demanding more-and better-apps',
+    'assets/images/webdevelopment/webdevelopment-104.jpg',
+    'Flutter in Action', 
+    'webdevelopment-104', 
+    1000.00,
+    100,
+    NULL,
+    1
+);
+insert into tbl_book VALUES 
+(
+	5,
+	true,
+	'2020-03-18',
+    'RabbitMQ in Depth is a practical guide to building and maintaining message-based applications. This book provides detailed',
+    'assets/images/webdevelopment/webdevelopment-105.jpg',
+    'RabbitMQ in Depth', 
+    'webdevelopment-105', 
+    500.00,
+    100,
+    NULL,
+    1
+);
+insert into tbl_book VALUES 
+(
+	6,
+	true,
+	'2020-03-18',
+    'Learn the fundamentals of version control through step-by-step tutorials that will teach you the ins-and-outs of Git. This book is your complete guide to',
+    'assets/images/webdevelopment/webdevelopment-106.jpg',
+    'Beginning Git and GitHub',
+    'webdevelopment-106', 
+    600.00,
+    100,
+    NULL,
+    1);
+insert into tbl_book VALUES 
+(
+	7,
+	true,
+	'2020-03-18', 
+    'Explore microservices using the Python-based Django framework and review the benefits and drawbacks of them. This book will examine what microservices look like',
+    'assets/images/webdevelopment/webdevelopment-107.jpg',
+    'Designing Microservices with Django', 
+    'webdevelopment-107', 
+    700.00,
+    100,
+    NULL,
+    1);
+insert into tbl_book VALUES 
+(
+	8,
+	true,
+	'2020-03-18',
+    'mmerse yourself in learning Python and introductory data analytics with this book’s project-based approach. Through the structure of a ten-week coding bootcamp course',
+    'assets/images/webdevelopment/webdevelopment-108.jpg',
+    'Python Projects for Beginners', 
+    'webdevelopment-108', 
+    800.00,
+    100,
+    NULL,
+    1);
+insert into tbl_book VALUES 
+(
+	9,
+	true,
+	'2020-03-18',
+    'Walter Savitch’s Problem Solving with C++” is one of the most widely used textbooks for the introductory programming in C++ course.',
+    'assets/images/programming/programming-100.jpg',
+    'Problem Solving with C++', 
+    'programming-100',
+    400.00,
+    100,
+    NULL,
+    2);
+insert into tbl_book VALUES 
+(
+	10,
+	true,
+	'2020-03-18',
+    'Discover the Jakarta EE Contexts and Dependency Injection (CDI 2.0) framework which helps you write better code through the use of well-defined enterprise',
+    'assets/images/programming/programming-101.jpg',
+    'Introducing Jakarta EE CDI', 
+    'programming-101',
+    500.00,
+    100,
+    NULL,
+    2);
+insert into tbl_book VALUES 
+(
+	11,
+	true,
+	'2020-03-18',
+    'This book is designed to introduce students to programming and computational thinking through the lens of exploring data. You can think of Python as your tool',
+    'assets/images/programming/programming-102.jpg',
+    'Python for Informatics',
+    'programming-102',
+    600.00,
+    100,
+    NULL,
+    2);
+insert into tbl_book VALUES 
+(
+	12,
+	true,
+	'2020-03-18',
+    'Harness the power of MATLAB for deep-learning challenges. This book provides an introduction to deep learning and using MATLAB’s deep-learning toolboxes.',
+    'assets/images/programming/programming-103.jpg',
+    'Practical MATLAB Deep Learning',
+    'programming-103',
+    700.00,
+    100,
+    NULL,
+    2
+);
+insert into tbl_book VALUES 
+(
+	13,
+	true,
+	'2020-03-18',
+    'Design and write simple and efficient T-SQL code in SQL Server 2019 and beyond. Writing T-SQL that pulls back correct results can be challenging.',
+    'assets/images/databases/databases-100.jpg',
+    'Pro T-SQL 2019', 
+    'databases-100',
+    700.00,
+    100,
+    NULL,
+    3
+);
+insert into tbl_book VALUES 
+(
+	14,
+	true,
+	'2020-03-18',
+    'Use this fast-paced and comprehensive guide to build cloud-based solutions on Oracle Cloud Infrastructure. You will understand cloud infrastructure',
+    'assets/images/databases/databases-101.jpg',
+    'Practical Oracle Cloud Infrastructure', 
+    'databases-101',
+    500.00,
+    100,
+    NULL,
+    3
+);
+insert into tbl_book VALUES 
+(
+	15,
+	true,
+	'2020-03-18',
+    'Modern data science solutions need to be clean, easy to read, and scalable. In Mastering Large Datasets with Python, author J.T. Wolohan',
+    'assets/images/databases/databases-102.jpg',
+    'Mastering Large Datasets with Python',
+    'databases-102',
+    600.00,
+    100,
+    NULL,
+    3
+);
+insert into tbl_book VALUES 
+(
+	16,
+	true,
+	'2020-03-18',
+    'Get a head-start on learning one of SQL Server 2019’s latest and most impactful features―Big Data Clusters―that combines large volumes of non-relational data',
+    'assets/images/databases/databases-103.jpg',
+    'SQL Server Big Data Clusters',
+    'databases-103',
+    700.00,
+    100,
+    NULL,
+    3
+);
+insert into tbl_book VALUES 
+(
+	17,
+	true,
+	'2020-03-18',
+    'A clear, complete guide to getting the most from cPanel. Anybody who has a web hosting account that provides cPanel, and who wants to get the most from this very powerful tool.',
+    'assets/images/databases/administration-100.jpg',
+    'cPanel User Guide and Tutorial',
+    'administration-100', 
+    700.00,
+    100,
+    NULL,
+    4
+);
+insert into tbl_book VALUES 
+(
+	18,
+	true,
+	'2020-03-18',
+    'Written by one of the most popular and knowledgeable Microsoft Outlook MVPs, this book fills a void in the market for a professional-level Outlook programming book',
+    'assets/images/databases/administration-101.jpg',
+    'Professional Outlook 2007 Programming', 
+    'administration-101', 
+    500.00,
+    100,
+    NULL,
+    4
+);
+insert into tbl_book VALUES 
+(
+	19,
+	true,
+	'2020-03-18',
+    'Build clustered and scalable Java-based, real-time applications using Spring Framework, Boot, WebSocket, Cassandra, Redis and RabbitMQ. In this book',
+    'assets/images/databases/administration-102.jpg',
+    'Pro Java Clustering and Scalability', 
+    'administration-102', 
+    600.00,
+    100,
+    NULL,
+    4
+);
+insert into tbl_book VALUES 
+(
+	20,
+	true,
+	'2020-03-18',
+    'Learn the basic principles of modular design, and then put them into action to create sites that are easy to use, look great.',
+    'assets/images/databases/administration-103.jpg',
+    'Modular Design Frameworks', 
+    'administration-103', 
+    700.00,
+    100,
+    NULL,
+    4
+);
